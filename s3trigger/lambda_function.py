15adc13ab7e59f892c3f0db03cb59d306a6774ad
Chetan Gaikar <chetangaikar@gmail.com>
2022-09-01 14:44:31 +0530

    CHANGES

--- s3trigger/lambda_function.py
+++ s3trigger/lambda_function.py
@@ -10,6 +10,8 @@ s3 = boto3.client('s3')
 def lambda_handler(event, context):
     #print("Received event: " + json.dumps(event, indent=2))
 
+    #*****THIS IS THE FIRST FUNCTION*****
+
     # Get the object from the event and show its content type
     bucket = event['Records'][0]['s3']['bucket']['name']
     key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
